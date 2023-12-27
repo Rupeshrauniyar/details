a92bace8c23fe6e341e6a171fca2616cb5ea21b5
Rupeshrauniyar <151705269+Rupeshrauniyar@users.noreply.github.com>
2023-12-28 00:06:25 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+function emailSend(){
+
+var userNameone = document.getElementById('fname').value;
+var userNametwo = document.getElementById('sname').value;
+var address = document.getElementById('address').value;
+var phone = document.getElementById('number').value;
+var email = document.getElementById('email').value;
+
+var messageBody ="First Name -" + userNameone +
+"<br/> Second Name- " + userNametwo +
+"<br/> Address- " + address +
+"<br/> Phone- " + phone +
+"<br/> Email- " + email;
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "rupeshrauniyar80@gmail.com",
+    Password : "58AA305153294CB96DBF7F0BA477958ADFAD",
+    To : 'rupeshrauniyaryt@gmail.com',
+    From : "rupeshrauniyar80@gmail.com",
+    Subject : "Gorkhali Squats Booking",
+    Body : messageBody
+}).then(
+  message => alert("Your Details Have Been Submitted Successfully ")
+  
+
+
+);
+}
+
 function sendOTP() {
 const email = document.getElementById('email')
 const otpverify = document.getElementsByClassName('otpverify')[0];
@@ -7,15 +36,15 @@ let emailbody = `<h2>Your OTP Is</h2>${otp_val}`;
 
 
 Email.send({ 
-SecureToken : "b4ece08c-85af-42b9-8e17-daa499ac9efe", 
+SecureToken : "e1f19748-e352-4a6f-85c7-1468d8893194", 
 To : email.value, 
 From : "rupeshrauniyar80@gmail.com", 
 Subject : "Email OTP", 
 Body : emailbody ,
-}).then( 
+}) .then( 
 
 message => {
-if (message === "OK") {
+if (message === "OK"){
  alert("OTP Sent To Your Email");
 
 otpverify.style.display="grid";
